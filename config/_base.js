1e2abec667d6ae2e94dfece9e6f69e2d84c6452d
Nicolas Carlier <n.carlier@nunux.org>
2016-04-11 18:49:23 +0200

    fix(conf): Fix configuration.

--- config/_base.js
+++ config/_base.js
@@ -19,7 +19,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : 'localhost',
+  server_host : 'home',
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
