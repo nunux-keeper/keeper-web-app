ef8d6ef31ea3ad02be1215a9645eac12e194d45a
Nicolas Carlier <n.carlier@nunux.org>
2016-07-04 20:57:40 +0200

    fix(): Small bug fixes.

--- src/views/NotFoundView/NotFoundView.jsx
+++ src/views/NotFoundView/NotFoundView.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export const NotFoundView = (props) => (
-  <div className='ui segment'>
-    <div className='ui dimmer warn'>
+  <div className='ui segment' style='height: 100%'>
+    <div className='ui active dimmer warn'>
       <div className='content'>
         <div className='center'>
           <h2 className='ui inverted icon header'>
