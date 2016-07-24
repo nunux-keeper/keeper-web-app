f933fb3a9ab56ae8f4d2d7d223dbd859f5aa8fc8
Nicolas Carlier <n.carlier@nunux.org>
2016-07-24 14:22:05 +0200

    feat(home): Minimal style for the home page.

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export const HomeView = (props) => (
-  <div>
-    <h1>Welcome to Nunux Keeper</h1>
-    <hr />
-    <Link to='/document'>View my documents</Link>
+  <div id='welcome' className='ui panel announcement'>
+    <div className='content'>
+      <div className='center'>
+        <h2 className='ui icon header'>
+          <img src='https://keeper.nunux.org/icons/icon_152.png' className='ui image' />
+          <div className='content'>
+            Nunux Keeper v2
+            <div className='sub header'>
+              Your personal content curation service
+            </div>
+          </div>
+        </h2>
+        <br/>
+        <Link to='/document' className='positive ui button'>
+          View my documents
+        </Link>
+      </div>
+    </div>
   </div>
 )
 
